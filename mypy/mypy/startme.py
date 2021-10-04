fbdab218b3d6215c6b323d45b141a751abff4446
Ben Greene <BenJGreene@gmail.com>
2021-10-04 21:04:12 +0100

    Create async functions and futures to follow them

--- mypy/mypy/startme.py
+++ mypy/mypy/startme.py
@@ -1,6 +1,10 @@
 import asyncio
 import time
 
+
+running = True
+
+
 def start():
     print("i am going to sleep")
     time.sleep(5)
@@ -11,3 +15,18 @@ async def as_start():
     print("i am going to async sleep")
     await asyncio.sleep(5)
     print("I got an async sleep done")
+
+
+async def as_start_loop():
+    print("i am going to start now")
+    while(running):
+        await asyncio.sleep(1)
+        print(".. still here")
+    print("Python has completed")
+
+async def as_stop():
+    global running
+    print("Stopping loop")
+    await asyncio.sleep(5)
+    print("Actuating stop")
+    running = False
--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,8 @@ use env_logger::Env;
 use log::{info};
 use pyo3::prelude::*;
 
+use futures::future::join_all;
+
 #[pyo3_asyncio::tokio::main]
 async fn main() -> PyResult<()> {
     env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
@@ -13,22 +15,39 @@ async fn main() -> PyResult<()> {
     info!("Calling start");
 
 
-    let fut0 = Python::with_gil(|py| {
-        let asyncio = py.import("asyncio")?;
-        // convert asyncio.sleep into a Rust Future
-        pyo3_asyncio::tokio::into_future(asyncio.call_method1("sleep", (1.into_py(py),))?)
-    })?;
+    // let fut0 = Python::with_gil(|py| {
+    //     let startme = py.import("mypy.startme")?;
+    //     // convert asyncio.sleep into a Rust Future
+    //     pyo3_asyncio::tokio::into_future(startme.call_method0("as_start_loop")?)
+    // })?;
 
     let fut1 = Python::with_gil(|py| {
-        let startme = py.import("mypy.startme")?;
-        pyo3_asyncio::tokio::into_future(startme.call_method0("as_start")?)
+        let asyncio = py.import("asyncio")?;
+        // convert asyncio.sleep into a Rust Future
+        pyo3_asyncio::tokio::into_future(asyncio.call_method1("sleep", (5.into_py(py),))?)
     })?;
+
+    let mut fut_list = Python::with_gil(|py| {
+        let startme = py.import("mypy.startme").expect("");
+        let sub0 = pyo3_asyncio::tokio::into_future(startme.call_method0("as_stop").expect("")).expect("");
+        let sub1 = pyo3_asyncio::tokio::into_future(startme.call_method0("as_start_loop").expect("")).expect("");
+        // let retval = vec![sub0?, sub1?];
+        // futures::future::join_all(vec![sub0, sub1])
+        // Ok(vec![sub0, sub1])
+        // Ok(vec![])
+        vec![sub0, sub1]
+    });
+
+    // let mut  fut_list =vec![fut2];
+
+    fut_list.push(fut1);
 
     info!("Have requested python function");
 
-
-    fut0.await?;
-    fut1.await?;
+    join_all(fut_list).await;
+    // fut0.await?;
+    // fut1.await?;
+    // fut2.await?;
 
     info!("Completed all");
 
